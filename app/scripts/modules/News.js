d041decff90adacda7639170d3905b092eb03c26
Thomas Mery <thomas.mery@gmail.com>
2013-01-19 17:51:50 +0100

    added radio streams

--- app/scripts/modules/News.js
+++ app/scripts/modules/News.js
@@ -17,7 +17,13 @@ function(app) {
 
     console.log('NEWS INIT');
 
-    var layout = new News.Views.Layout();
+    var layout;
+
+    var audiostreams = new News.AudioStreams();
+    audiostreams.on('reset', function() {
+      layout = new News.Views.Layout({audiostreams: this});
+    });
+    audiostreams.fetchData();
 
   }
 
@@ -26,9 +32,100 @@ function(app) {
   
   });
 
+  // AudioStream Model
+  News.AudioStream = News.Model.extend({
+
+  });
+
   // Default Collection.
   News.Collection = Backbone.Collection.extend({
-    model: News.Model
+    
+    model: News.Model,
+    currentStream: 0,
+
+    // fetch data for this collection
+    // uses the 'type' property to build data url
+    fetchData: function() {
+
+      var _this = this;
+
+      if(typeof this.type == "undefined") {
+        throw "News Collection must have a type to be able to fetch data"
+      }
+
+      $.get('data/news-'+this.type+'.txt').done(
+
+        function(data) { 
+
+          _this.reset($.parseJSON(data));
+
+        }
+      );
+
+    }
+
+  });
+
+  // AudioStreams Collection
+  News.AudioStreams = News.Collection.extend({
+    type: "audiostreams",
+    model: News.AudioStream
+  });
+
+  // AudioStream View
+  // this is just a Html Audio Element
+  News.Views.AudioStream = Backbone.LayoutView.extend({
+
+    popcorn: null,
+
+    initialize: function() {
+
+      var view = this;
+
+      this.popcorn = new Popcorn.HTMLAudioElement(this.el);
+      this.popcorn.src = this.model.get('url');
+
+      this.model.on('change:url', this.change, this);
+
+      this.$el.append('<button>Change Radio</button>');
+      this.$el.append('<div class="infos"></div>');
+
+
+      this.$el.find('button').on('click', function() {
+
+        view.collection.currentStream++;
+
+        var nextStream = view.collection.at(view.collection.currentStream);
+
+        view.model.set('name', nextStream.get('name'));
+        view.model.set('url', nextStream.get('url'));
+
+        
+        
+      });
+
+      this.change(this.model);
+
+    },
+
+    change: function(model) {
+      console.log(model.get('name'));
+      this.popcorn.src = model.get('url');
+      this.$el.find('.infos').html(model.get('name'));
+      this.pause();
+      this.play();
+    },
+
+    // wrapper for popcorn instance play method
+    play: function() {
+      this.popcorn.play();
+    },
+
+    // wrapper for popcorn instance pause method
+    pause: function() {
+      this.popcorn.pause();
+    }
+
   });
 
   // Default View.
@@ -50,12 +147,20 @@ function(app) {
 
      initialize: function() {
 
+      var audiostreams = this.options.audiostreams
+      ,   audiostreamView
+
       this.$el.css({height: '100%'});
 
       $('#module-container').css({opacity: 0});
 
       // add layout to the dom
       $('#module-container').empty().append(this.el);
+
+     
+      // set AudioStream View
+      // one View to listen to them all
+      window.as = audiostreamView = this.setView('#audiostream', new News.Views.AudioStream({model: audiostreams.at(0), collection: audiostreams}))
 
       // render layout
       this.render();
