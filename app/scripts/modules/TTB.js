9eef4723c11988dca8c553e535f646a83152686b
Thomas Mery <thomas.mery@gmail.com>
2013-03-09 22:59:33 +0100

    'back to ttb' style change

--- app/scripts/modules/TTB.js
+++ app/scripts/modules/TTB.js
@@ -142,7 +142,7 @@ function(app, Video, Soundtrack) {
 
         var v = this
         ,   vv = this.vv
-        ,   bt = this.back_to_ttb_button =  $('<button class="btn btn-inverse">Poursuivre l\'exploration</button>');
+        ,   bt = this.back_to_ttb_button =  $('<button class="btn">Poursuivre l\'exploration</button>');
 
         // append home button
         bt.css({ 
